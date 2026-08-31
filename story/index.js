e24466f6487d635b3f796da57195e58d3d7be363
Sean Panacides <sean@192.168.1.131>
2026-08-31 09:59:24 +0100

    refactor: add individual files for locations and landing page

---
+++ story/index.js
@@ -0,0 +1,9 @@
+import { introNodes } from './intro.js'
+import { phoneNodes } from './phone.js'
+import { elfNodes } from './elf.js'
+
+export const textNodes = [
+  ...introNodes,
+  ...phoneNodes,
+  ...elfNodes,
+]
